63608c6c305d3996c4ba4580a837205505521202
Kelly Luce <kellyeluce@gmail.com>
2014-09-29 22:46:00 -0700

    styled page, formated currency

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,10 @@
 (function(){
+	$("form").parsley();
 	$("form").on({
 		submit:function(e){
 			e.preventDefault();
+			$(".spinner").remove();
+			$("body").append('<i class="fa fa-cog fa-spin  fa-5x spinner"></i>');
 
 			//placing variable in global scope so jsonp is used
 			converter = $("#CurrencyConverter").val();
@@ -25,7 +28,14 @@
 	})
 
 	window.jsonCallback = function(data) {
-			var total=(data.BTC.USD*converter).toFixed(2);
+			var total=(data.BTC.USD*converter);
+
+			$(".spinner").remove();
+
+			$("#conversion")
+				.html(total)
+				.formatCurrency(total)
+				.prepend("Your Bitcoins are worth ");
 
 
 			console.log(total);
