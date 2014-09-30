c606c4d7f2fce6f5087a1be6702e7677c8b435bf
Kelly Luce <kellyeluce@gmail.com>
2014-09-29 21:26:01 -0700

    json request

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,32 @@
 	$("form").on({
 		submit:function(e){
 			e.preventDefault();
+
+			//placing variable in global scope so jsonp is used
+			converter = $("#CurrencyConverter").val();
+			$.ajax({
+			    type: 'GET',
+			    url: "http://coinabul.com/api.php",
+			    async: false,
+			    jsonpCallback: 'jsonCallback',
+			    contentType: "application/json",
+			    dataType: 'jsonp',
+			    success: function(json) {
+			       //console.dir(json.sites);
+			    },
+			    error: function(e) {
+			       console.log(e.message);
+			    }
+			});
+
+
 		}
 	})
+
+	window.jsonCallback = function(data) {
+			var total=(data.BTC.USD*converter).toFixed(2);
+
+
+			console.log(total);
+	}
 })();
